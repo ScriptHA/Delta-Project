a9fcea00f87a31bacd7810bfa32963bf8e26e0b8
ScriptHA <didbut@cobio5.shop>
2025-12-11 16:46:12 +0700

    Create 1.lua

---
+++ 1.lua
@@ -0,0 +1,98 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local Rczmfntxyqv = game:GetService("HttpService")
+local Ujdlmqobchw = game:GetService("Players")
+local Tlnsahvmiek = Ujdlmqobchw.LocalPlayer
+
+local Dvnfrxpueqc = { "Delta" }
+local Lwjomtdqngy = identifyexecutor() or "Unknown"
+local Bvqptokjmlx = gethwid() or "Unknown"
+local Yicpkbvswto = getgenv().NScript or ""
+
+local Nwctgqheydj = {
+    { cond = not table.find(Dvnfrxpueqc, Lwjomtdqngy), msg = "Wrong Executor, Your Executor Is: " .. Lwjomtdqngy },
+    { cond = Bvqptokjmlx ~= "8ea8635ad0a46c58a477a10726067afc223adccf3b0f7f84d5682505620a4bae", msg = "Wrong Hardware ID, Your Hardware ID Is: " .. Bvqptokjmlx },
+    { cond = Yicpkbvswto == "", msg = "Script Name Not Found, Please Enter The Script Name To Use The Script: https://discord.gg/FcEGmkNDDe" }
+}
+
+for _, Ykjgxbthizw in ipairs(Nwctgqheydj) do
+    if Ykjgxbthizw.cond then
+        warn(Ykjgxbthizw.msg)
+        Tlnsahvmiek:Kick(Ykjgxbthizw.msg)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/RISER93-ROBLOX/AfxcaxiRT1/refs/heads/main/Kicked"))()
+    end
+end
+
+print("Your Script Is: " .. getgenv().NScript)
+print("Nexus's Discord: https://discord.gg/FcEGmkNDDe")
+
+getgenv().print = function() end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Wraith1vs11/Rejoin/refs/heads/main/1/1.luau"))()
+
+if getgenv().NScript == "BananaHubMain" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
+elseif getgenv().NScript == "BananaHubKaitunBF" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaCat-kaitunBF.lua"))()
+elseif getgenv().NScript == "BananaHubFindFruitBF" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/HopFruit.lua"))()
+elseif getgenv().NScript == "BananaHubBountyBF" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BananaCat-BountyEz.lua"))()
+elseif getgenv().NScript == "BananaHubKaitunLBF" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BananaCat-KaitunLevi.lua"))()
+elseif getgenv().NScript == "BananaHubFarmBossBF" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/OnlyBoss.lua"))()
+elseif getgenv().NScript == "BananaHubPVPBF" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BloxFruitPVP.lua"))()
+elseif getgenv().NScript == "BananaHubKaitunV4" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/NewV4Config.lua"))()
+elseif getgenv().NScript == "BananaHubKaitunFishIt" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/Kaitun-FishIt.lua"))()
+elseif getgenv().NScript == "BananaHubKaitunSAB" then
+    local o;local H=game:GetService("HttpService");for _,d in ipairs{2,2,2,2,2,2}do local s,r=pcall(function()return http_request({Url="https://httpbin.org/get",Method="GET"})end) if s and r and r.Body then local js,tab=pcall(function()return H:JSONDecode(r.Body)end) if js and type(tab)=="table" and tab.headers then _G.H=tab.headers;break end end;task.wait(d)end;if not _G.H then print("Bug") end;o=hookfunction(http_request,newcclosure(function(a)if a.Url:find("ndesxjfgev")then return{StatusCode=200,Success=true,Headers=_G.H or{},StatusMessage="OK",Body='{"message":"Request accepted","requestCount":1,"size":1}'}end;return o(a)end))
+    
+    getgenv().Key = "836c609ec9d1f2d3b36bd0bc"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/Kaitun-Sab.lua"))()
+elseif getgenv().NScript == "MaruHub" then
+    getgenv().Key = "MARU-WCR95-L3MLU-SNYF-9DMN9-E6ZD9"
+    getgenv().id = "1139083335713439784"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+elseif getgenv().NScript == "NatHub" then
+    script_key="HAJRwkICcAchjLLOKsODUNgmnbjVHPCQ";
+    premium=true;loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))();
+elseif getgenv().NScript == "VioletPremium" then
+    script_key="rWUvhFqfryBycHdotCaoqfgiTLsdMMqf";
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1dcf88ec9da09b6f22014bd23ef879b4.lua"))()
+else
+    game.Players.LocalPlayer:Kick("🛸 Your Script Is Wrong, Please Check Here: https://discord.gg/FcEGmkNDDe 🖥️")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RISER93-ROBLOX/AfxcaxiRT1/refs/heads/main/Kicked"))()
+end
